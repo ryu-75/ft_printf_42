8ab5971f7d30921d5acf550d6d348d17d854b0d9
Nicolas LORION <nlorion.mail@gmail.com>
2022-05-18 12:28:13 +0200

    added and midified some tuff

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,11 @@
 /*   By: nlorion <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 20:11:14 by nlorion           #+#    #+#             */
-/*   Updated: 2022/05/17 21:12:04 by nlorion          ###   ########.fr       */
+/*   Updated: 2022/05/18 12:10:11 by nlorion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "libft/libft.h"
 
 typedef struct	s_format
 {
@@ -46,10 +46,24 @@ int	ft_printf(const char *format, ...)
 {
 	int	i;
 	char	*tab;
-	
-	// initalise le malloc avec la taille de la structure
-	// si tab n existe pas retourne NULL
-	// initialise tab avec ft_initialise_tab
-	va_arg(tab->args, format); 	// initialisation  
-		
+	int	res;
+
+	tab = (t_format *)malloc(sizeof(t_format));	// initalise le malloc avec la taille de la structure
+	if (!tab)
+		return (NULL);				// si tab n existe pas retourne NULL
+	ft_initialise_tab(tab);				// initialise tab avec ft_initialise_tab
+	va_arg(tab->args, format); 			// initialisation
+	i = -1;
+	res = 0;
+	while (format[i++])
+	{
+		if (format[i] == '%')
+			res = write(1, '%', 1);
+	}		
+	return (res);
+}
+
+int	main()
+{
+	ft_printf('%');
 }
