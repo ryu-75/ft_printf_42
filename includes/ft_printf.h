7d9f5d8584c2fae01c961cff5ce0732fa08bce8d
Nicolas LORION <nlorion.mail@gmail.com>
2022-05-20 18:00:53 +0200

    update

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -7,14 +7,29 @@
 # include <unistd.h>
 
 size_t	ft_putnb(int n);
+
 size_t	ft_putchar(char c);
+
 size_t	ft_strprint(const char *str);
-void	ft_uputnb(unsigned int n);
+
+size_t	ft_uputnb(unsigned long long n);
+
 size_t	ft_puthex_low(unsigned int n);
+
 size_t	ft_puthex_up(unsigned int n);
+
+size_t	ft_puthexptr(unsigned long long int n);
+
+size_t	ft_printptr(unsigned long long n);
+
+size_t	ft_hexalen(unsigned long long int n);
+
+size_t	ft_numlen(int n);
+
 void	ft_putstr(char *str);
 
 int	ft_strlen(char *str);
+
 int	ft_printf(char const *format, ...);
 
 # endif
