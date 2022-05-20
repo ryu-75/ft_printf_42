397c82c83b6ba5d38f1bf2320e44783b4a8340eb
Nicolas LORION <nlorion.mail@gmail.com>
2022-05-20 18:01:34 +0200

    Need to fixed

--- ft_printf.c
+++ ft_printf.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+ear* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
@@ -6,7 +6,7 @@
 /*   By: nlorion <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 16:13:17 by nlorion           #+#    #+#             */
-/*   Updated: 2022/05/20 12:43:45 by nlorion          ###   ########.fr       */
+/*   Updated: 2022/05/20 17:58:46 by nlorion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,25 @@ size_t	ft_arg(va_list args, char const *c)
 	size_t	ret;
 
 	i = 0;
+	ret = 0;
 	if (c[i] == 'c')
 		ret += (ft_putchar(va_arg(args, int)));
 	else if (c[i] == 's')
 		ret += (ft_strprint(va_arg(args, char *)));
-	else if (c[i] == 'i' || c[i] == 'u')
+	else if (c[i] == 'i')
 		ret += (ft_putnb(va_arg(args, int)));
+	else if (c[i] == 'd')
+		ret += (ft_putnb(va_arg(args, long long int)));
+	else if (c[i] == 'u')
+		ret += (ft_uputnb(va_arg(args, long long int)));
 	else if (c[i] == 'x')
 		ret += (ft_puthex_low(va_arg(args, unsigned int)));
 	else if (c[i] == 'X')
 		ret += (ft_puthex_up(va_arg(args, unsigned int)));
+	else if (c[i] == 'p')
+		ret += (ft_printptr(va_arg(args, unsigned long long int)));
+	else if (c[i] == '%')
+		ret += (ft_putchar('%'));
 	return (ret);
 }
 
@@ -55,8 +64,11 @@ int	ft_printf(char const *format, ...)
 	va_end(args);
 	return (ret);
 }
-
+/*
 int	main()
 {
-	ft_printf("string : %s\nchar : %c\nnumber : %i\nlow hexa : %x\nup hexa : %X\n", "bonjour", 't', 42, 45123, 45123);
-}
+	int	n = 45123;
+	int	*p = &n;
+	//printf("string : %s\nchar : %c\nnumber : %i\nunsigned number : %u\ndecimal num : %d\nlow hexa : %x\nup hexa : %X\npointeur : %p\npercent : %%\n", "bonjour", 't', 42, 43, 44, n, n, p);
+	ft_printf("string : %s\nchar : %c\nnumber : %i\nunsigned number : %u\ndecimal num : %d\nlow hexa : %x\nup hexa : %X\npointeur : %p\npercent :  %%\n", "bonjour", 't', 42, 43, 44, n, n, p);
+}*/
